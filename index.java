60c88c1c745e78f569c31770c72847c2af4132fc
veera-m4 <100131144+veera-m4@users.noreply.github.com>
2022-05-25 17:22:47 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,23 +1,11 @@
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
-import org.apache.lucene.analysis.core.SimpleAnalyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.*;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.*;
 import org.apache.lucene.util.Version;
-import org.apache.lucene.search.IndexSearcher;
 import  org.apache.lucene.document.*;
 import java.io.*;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import java.util.*;
 
 
 class LuceneTester {
